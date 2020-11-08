3d6629810b43e1ffab05f70fba364ae571d076ea
erikos93 <erikwaahlstrom@gmail.com>
2020-11-08 19:39:35 +0100

    moved shuffleArray function logic outside of quiz container component

--- src/components/QuizContainer.js
+++ src/components/QuizContainer.js
@@ -4,59 +4,76 @@ import { CounterComponent } from "../components";
 // Elements
 import { HeaderOne, Wrapper, OptionsWrapper, Option } from "../elements";
 
-export const QuizContainer = () => {
-  const questions = [
-    {
-      questionText: "What is the capital of France?",
-      answerOptions: [
-        { option: "New York", isCorrect: false },
-        { option: "London", isCorrect: false },
-        { option: "Paris", isCorrect: true },
-        { option: "Dublin", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Who is CEO of Tesla?",
-      answerOptions: [
-        { option: "Jeff Bezos", isCorrect: false },
-        { option: "Elon Musk", isCorrect: true },
-        { option: "Bill Gates", isCorrect: false },
-        { option: "Tony Stark", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "The iPhone was created by which company?",
-      answerOptions: [
-        { option: "Apple", isCorrect: true },
-        { option: "Intel", isCorrect: false },
-        { option: "Amazon", isCorrect: false },
-        { option: "Microsoft", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "How many Harry Potter books are there?",
-      answerOptions: [
-        { option: "1", isCorrect: false },
-        { option: "4", isCorrect: false },
-        { option: "6", isCorrect: false },
-        { option: "7", isCorrect: true },
-      ],
-    },
-  ];
+const questions = [
+  {
+    questionText: "What is the capital of France?",
+    answerOptions: [
+      { option: "New York", isCorrect: false },
+      { option: "London", isCorrect: false },
+      { option: "Paris", isCorrect: true },
+      { option: "Dublin", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Who is CEO of Tesla?",
+    answerOptions: [
+      { option: "Jeff Bezos", isCorrect: false },
+      { option: "Elon Musk", isCorrect: true },
+      { option: "Bill Gates", isCorrect: false },
+      { option: "Tony Stark", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "The iPhone was created by which company?",
+    answerOptions: [
+      { option: "Apple", isCorrect: true },
+      { option: "Intel", isCorrect: false },
+      { option: "Amazon", isCorrect: false },
+      { option: "Microsoft", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "How many Harry Potter books are there?",
+    answerOptions: [
+      { option: "1", isCorrect: false },
+      { option: "4", isCorrect: false },
+      { option: "6", isCorrect: false },
+      { option: "7", isCorrect: true },
+    ],
+  },
+];
 
+const arrayShuffle = function (arr) {
+  let newPos, temp;
+  for (let i = arr.length - 1; i > 0; i--) {
+    newPos = Math.floor(Math.random() * (i + 1));
+    temp = arr[i];
+    arr[i] = arr[newPos];
+    arr[newPos] = temp;
+  }
+  return arr;
+};
+
+const newArray = arrayShuffle(questions);
+console.log(newArray);
+
+////////////////////////////////////////////////////////////
+////////// QuizContainer Component /////////////////////////
+////////////////////////////////////////////////////////////
+export const QuizContainer = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
   // Counter
   const [counter, setCounter] = useState(0);
 
-  // useEffect(() => {
-  //   const interval = setInterval(
-  //     () => setCounter((counter) => counter + 1),
-  //     1000
-  //   );
-  //   return () => clearInterval(interval);
-  // }, []);
+  useEffect(() => {
+    const interval = setInterval(
+      () => setCounter((counter) => counter + 1),
+      1000
+    );
+    return () => clearInterval(interval);
+  }, []);
 
   // Event handlers
   const handleAnswerOptionClick = (isCorrect) => {
@@ -73,20 +90,6 @@ export const QuizContainer = () => {
   const nextQuestion = () => {
     setCounter(0);
   };
-
-  const arrayShuffle = function (arr) {
-    let newPos, temp;
-    for (let i = arr.length - 1; i > 0; i--) {
-      newPos = Math.floor(Math.random() * i + 1);
-      temp = arr[i];
-      arr[i] = arr[newPos];
-      arr[newPos] = temp;
-    }
-    return arr;
-  };
-
-  const newArray = arrayShuffle(questions);
-  console.log(newArray);
 
   return (
     <Wrapper>
